6dae2015eaf2c5aa7ed959f55ff759d04adb0142
Charlie Vieth <charlie.vieth@gmail.com>
2018-03-22 19:00:50 -0400

    WIP: walk - working walk function

--- walk/main.go
+++ walk/main.go
@@ -1,18 +1,26 @@
 package main
 
 import (
+	"archive/tar"
+	"bufio"
+	"bytes"
+	"compress/gzip"
 	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"runtime"
+	"sync"
+	"sync/atomic"
+	"time"
 )
 
 // TraverseLink is a sentinel error for Walk, similar to filepath.SkipDir.
 var TraverseLink = errors.New("traverse symlink, assuming target is a directory")
 
-var SkipFiles = errors.New("skip files")
-
-type WalkFn func(path string, typ os.FileMode) error
+type WalkFn func(path string, typ os.FileMode, rc io.ReadCloser) error
 
 // Walk walks the file tree rooted at root, calling walkFn for
 // each file or directory in the tree, including root.
@@ -28,27 +36,21 @@ type WalkFn func(path string, typ os.FileMode) error
 //   * Walk can follow symlinks if walkFn returns the TraverseLink
 //     sentinel error. It is the walkFn's responsibility to prevent
 //     Walk from going into symlink cycles.
-func Walk(root string, walkFn func(path string, typ os.FileMode) error) error {
+func Walk(root string, walkFn func(path string, typ os.FileMode, rc io.ReadCloser) error) error {
 	// TODO(bradfitz): make numWorkers configurable? We used a
 	// minimum of 4 to give the kernel more info about multiple
 	// things we want, in hopes its I/O scheduling can take
 	// advantage of that. Hopefully most are in cache. Maybe 4 is
 	// even too low of a minimum. Profile more.
 	//
-	// CEV: More than 10 workers and performance degrades.
 	numWorkers := 4
 	if n := runtime.NumCPU(); n > numWorkers {
-		//
-		if n > 10 {
-			numWorkers = 10
-		} else {
-			numWorkers = n
-		}
+		numWorkers = n
 	}
-	return walkN(root, walkFn, numWorkers)
+	return walkN(filepath.Clean(root), walkFn, numWorkers)
 }
 
-func walkN(root string, walkFn func(path string, typ os.FileMode) error, numWorkers int) error {
+func walkN(root string, walkFn func(path string, typ os.FileMode, rc io.ReadCloser) error, numWorkers int) error {
 	w := &walker{
 		fn:       walkFn,
 		enqueuec: make(chan walkItem, numWorkers), // buffered for performance
@@ -110,13 +112,13 @@ func (w *walker) doWork() {
 		case <-w.donec:
 			return
 		case it := <-w.workc:
-			w.resc <- w.walk(it.dir, !it.callbackDone)
+			w.resc <- w.walk(it.dir, !it.callbackDone, it.archive)
 		}
 	}
 }
 
 type walker struct {
-	fn func(path string, typ os.FileMode) error
+	fn func(path string, typ os.FileMode, rc io.ReadCloser) error
 
 	donec    chan struct{} // closed on Walk's return
 	workc    chan walkItem // to workers
@@ -126,6 +128,7 @@ type walker struct {
 
 type walkItem struct {
 	dir          string
+	archive      io.ReadCloser
 	callbackDone bool // callback already called; don't do it again
 }
 
@@ -146,8 +149,16 @@ func (w *walker) onDirEnt(dirName, baseName string, typ os.FileMode) error {
 		w.enqueue(walkItem{dir: joined})
 		return nil
 	}
+	if extArchive(joined) {
+		f, err := openFile(joined)
+		if err != nil {
+			return err
+		}
+		w.enqueue(walkItem{dir: joined, archive: f})
+		return nil
+	}
 
-	err := w.fn(joined, typ)
+	err := w.fn(joined, typ, nil)
 	if typ == os.ModeSymlink {
 		if err == TraverseLink {
 			// Set callbackDone so we don't call it twice for both the
@@ -163,10 +174,38 @@ func (w *walker) onDirEnt(dirName, baseName string, typ os.FileMode) error {
 	return err
 }
 
-// CEV: Called on directories!
-func (w *walker) walk(root string, runUserCallback bool) error {
+func (w *walker) onTarEnt(dirName, entName string, r io.Reader) error {
+	if extArchive(entName) {
+		buf := newBuffer()
+		if _, err := buf.ReadFrom(r); err != nil {
+			bufferPool.Put(buf)
+			return err
+		}
+		if extGzip(entName) {
+			gz, err := gzip.NewReader(buf)
+			if err != nil {
+				bufferPool.Put(buf)
+				return err
+			}
+			w.enqueue(walkItem{dir: dirName, archive: gzipReadCloser{
+				buf: buf,
+				gz:  gz,
+			}})
+			return nil
+		}
+		w.enqueue(walkItem{dir: dirName, archive: bufferReadCloser{Buffer: buf}})
+		return nil
+	}
+	// CEV: do we want to check if we care about the file first?
+	return w.fn(entName, 0, ioutil.NopCloser(r))
+}
+
+func (w *walker) walk(root string, runUserCallback bool, archive io.ReadCloser) error {
+	if archive != nil {
+		return readArchive(root, archive, w.onTarEnt)
+	}
 	if runUserCallback {
-		err := w.fn(root, os.ModeDir)
+		err := w.fn(root, os.ModeDir, nil)
 		if err == filepath.SkipDir {
 			return nil
 		}
@@ -174,45 +213,224 @@ func (w *walker) walk(root string, runUserCallback bool) error {
 			return err
 		}
 	}
-
 	return readDir(root, w.onDirEnt)
 }
 
-// readDir calls fn for each directory entry in dirName.
-// It does not descend into directories or follow symlinks.
-// If fn returns a non-nil error, readDir returns with that error
-// immediately.
+func readArchive(dirName string, rc io.ReadCloser, fn func(dirName, entName string, r io.Reader) error) error {
+	defer rc.Close()
+	var err error
+	tr := tar.NewReader(rc)
+	for {
+		hdr, e := tr.Next()
+		if e != nil {
+			if e != io.EOF {
+				err = e
+			}
+			break
+		}
+		if hdr.Typeflag != tar.TypeReg || hdr.Size == 0 {
+			continue
+		}
+		if err = fn(dirName, hdr.Name, tr); err != nil {
+			break
+		}
+	}
+	return err
+}
+
+// readDir calls fn for each directory entry in dirName. It does not descend
+// into directories or follow symlinks. If fn returns a non-nil error, readDir
+// returns with that error immediately.
+//
+// As a special case it is permitted for dirName to be a regular file.
+//
 func readDir(dirName string, fn func(dirName, entName string, typ os.FileMode) error) error {
-	fis, err := readDirEnts(dirName)
+	f, err := os.Open(dirName)
 	if err != nil {
 		return err
 	}
-	skipFiles := false
-	for _, fi := range fis {
-		typ := fi.Mode() & os.ModeType
-		if skipFiles && typ == 0 {
-			continue
+	list, err := f.Readdir(-1)
+	// allow regular files
+	if err != nil {
+		fi, e := f.Stat()
+		f.Close()
+		if e == nil && fi.Mode().IsRegular() {
+			dir := filepath.Dir(dirName)
+			return fn(dir, fi.Name(), fi.Mode()&os.ModeType)
 		}
-		if err := fn(dirName, fi.Name(), typ); err != nil {
-			if err == SkipFiles {
-				skipFiles = true
-				continue
-			}
+		return err
+	}
+	f.Close()
+
+	for _, fi := range list {
+		if err := fn(dirName, fi.Name(), fi.Mode()&os.ModeType); err != nil {
 			return err
 		}
 	}
 	return nil
 }
 
-func readDirEnts(dirname string) ([]os.FileInfo, error) {
-	f, err := os.Open(dirname)
+func openFile(name string) (io.ReadCloser, error) {
+	f, err := os.Open(name)
 	if err != nil {
 		return nil, err
 	}
-	list, err := f.Readdir(-1)
-	f.Close()
-	if err != nil {
-		return nil, err
+	if extGzip(name) {
+		// gzip will wrap the reading in a bufio.Reader if it
+		// does not implement ByteReader, use a slightly larger
+		// buffer than the default size of 4096 to reduce the
+		// number of syscalls.
+		gz, err := gzip.NewReader(bufio.NewReaderSize(f, 64*1024))
+		if err != nil {
+			f.Close()
+			return nil, err
+		}
+		return gzipFileCloser{gz: gz, file: f}, nil
 	}
-	return list, nil
+	return f, nil
 }
+
+func extGzip(name string) bool {
+	return hasSuffix(name, ".gz") || hasSuffix(name, ".tgz")
+}
+
+func extArchive(name string) bool {
+	return hasSuffix(name, ".tar") || hasSuffix(name, ".tgz") ||
+		hasSuffix(name, ".tar.gz")
+}
+
+// hasSuffix tests whether the string s ends with suffix.  Same as
+// strings.HasSuffix, but with a shorter name.
+func hasSuffix(s, suffix string) bool {
+	return len(s) >= len(suffix) && s[len(s)-len(suffix):] == suffix
+}
+
+var bufferPool sync.Pool
+
+func newBuffer() *bytes.Buffer {
+	if v := bufferPool.Get(); v != nil {
+		b := v.(*bytes.Buffer)
+		b.Reset()
+		return b
+	}
+	return new(bytes.Buffer)
+}
+
+type bufferReadCloser struct {
+	*bytes.Buffer
+}
+
+func (b bufferReadCloser) Close() error {
+	if b.Buffer != nil {
+		bufferPool.Put(b.Buffer)
+	}
+	return nil
+}
+
+type gzipFileCloser struct {
+	gz   *gzip.Reader
+	file *os.File
+}
+
+func (g gzipFileCloser) Read(p []byte) (int, error) {
+	return g.gz.Read(p)
+}
+
+func (g gzipFileCloser) Close() error {
+	g.file.Close()
+	return g.gz.Close()
+}
+
+type gzipReadCloser struct {
+	buf *bytes.Buffer
+	gz  *gzip.Reader
+}
+
+func (g gzipReadCloser) Read(p []byte) (int, error) {
+	return g.gz.Read(p)
+}
+
+func (g gzipReadCloser) Close() error {
+	bufferPool.Put(g.buf)
+	return g.gz.Close()
+}
+
+// type WalkFn func(path string, typ os.FileMode, rc io.ReadCloser) error
+
+func main() {
+	const root = "/Users/charlie/Desktop/ditmars-logs/warp-drive/out_logs.tgz"
+	// const root = "/Users/charlie/Desktop/ditmars-logs/warp-drive"
+	var count int64
+	var nill int64
+	var errs int64
+	start := time.Now()
+	err := Walk(root, func(path string, typ os.FileMode, rc io.ReadCloser) error {
+		// if path == "/Users/charlie/Desktop/ditmars-logs/warp-drive/out_logs" {
+		// 	return filepath.SkipDir
+		// }
+		if rc != nil {
+			if _, err := ioutil.ReadAll(rc); err != nil {
+				fmt.Printf("error (%s): %s\n", path, err)
+				atomic.AddInt64(&errs, 1)
+			}
+		} else {
+			atomic.AddInt64(&nill, 1)
+		}
+		atomic.AddInt64(&count, 1)
+		// fmt.Println(path)
+		return nil
+	})
+	d := time.Since(start)
+	fmt.Println("Error:", err)
+	fmt.Println("Count:", count)
+	fmt.Println("Nill:", nill)
+	fmt.Println("Errors:", errs)
+	fmt.Println("Time:", d, d/time.Duration(count))
+	fmt.Println()
+}
+
+/*
+type lazyFile struct {
+	file *os.File
+	name string
+}
+
+func (l *lazyFile) lazyInit() error {
+	if l.file != nil {
+		return nil
+	}
+	var err error
+	l.file, err = os.Open(l.name)
+	return err
+}
+
+func (l *lazyFile) Read(p []byte) (int, error) {
+	if err := l.lazyInit(); err != nil {
+		return 0, err
+	}
+	return l.file.Read(p)
+}
+
+func (l *lazyFile) Close() error {
+	if l.file != nil {
+		return l.file.Close()
+	}
+	return nil
+}
+*/
+
+/*
+var gzipReaderPool sync.Pool
+
+func newGzipReader(r io.Reader) (*gzip.Reader, error) {
+	if v := gzipReaderPool.Get(); v != nil {
+		gz := v.(*gzip.Reader)
+		if err := gz.Reset(r); err != nil {
+			gzipReaderPool.Put(gz)
+			return nil, err
+		}
+		return gz, nil
+	}
+	return gzip.NewReader(r)
+}
+*/
